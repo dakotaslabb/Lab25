0dcf3020becf2f2d2a80f88507500e39cce00ff0
dakota <dakotaslabb@gmail.com>
2019-10-10 18:35:24 -0400

    first Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab25.Models;
+
+namespace Lab25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Purchase()
+        {
+            return View();
+        }
+
+        public IActionResult Recipte(Purchase p)
+        {
+            ViewBag.Name = p.Name;
+            ViewBag.Price = p.Price;
+            ViewBag.Quantity = p.Quantity;
+            ViewBag.Description = p.Description;
+            return View(p);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CreditCard(Purchase p)
+        {
+            ViewBag.Price = p.Price;
+            ViewBag.Quantity = p.Quantity;
+            ViewBag.Description = p.Description;
+            return View(p);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab25.Models
+{
+    public class Purchase
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+        public string Description { get; set; }
+
+        public Purchase()
+        {
+
+        }
+
+        public Purchase(string Name, double Price, double Quantity, string Description)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Quantity = Quantity;
+            this.Description = Description;
+        }
+    }
+}
